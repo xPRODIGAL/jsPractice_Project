fb08dd1c4330738247662024f79d4f589a609eb5
xr <a365856177@163.com>
2022-11-21 23:24:54 +0800

    动态修改css样式

---
+++ 05_侧边栏登录框/script.js
@@ -0,0 +1,19 @@
+const toggle = document.getElementById("toggle");
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const modal = document.getElementById("modal");
+
+// 导航栏显示
+toggle.addEventListener("click", () =>
+  document.body.classList.toggle("show-nav")
+);
+
+open.addEventListener("click", () => modal.classList.add("show-modal"));
+
+close.addEventListener("click", () => modal.classList.remove("show-modal"));
+
+window.addEventListener("click", e =>
+  // console.log(e.target)
+  
+  e.target == modal ? modal.classList.remove("show-modal") : false
+);
